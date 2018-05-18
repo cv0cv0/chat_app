e359445f14c0b7bf5e58b5e57dc37b4989ef8cb7
cv0cv0 <linux2.0@foxmail.com>
2018-05-18 11:05:56 +0800

    Initial commit

---
+++ lib/const/const.dart
@@ -0,0 +1,1 @@
+const defaultUsername = 'John Doe';
---
+++ lib/style/style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final iosTheme = ThemeData(
+  primarySwatch: Colors.red,
+  primaryColor: Colors.grey,
+  primaryColorBrightness: Brightness.dark,
+);
+
+final androidTheme = ThemeData(
+  primarySwatch: Colors.blue,
+  accentColor: Colors.green,
+);
